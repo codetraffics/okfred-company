60828ee0ca3a30e281a4584b1e9f56890b4a0ba9
codetraffics <eboat086@gmail.com>
2024-03-20 16:55:57 +0000

    commit made

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 import AOS from "aos";
-// import "aos/dist/aos.css";
+import "aos/dist/aos.css";
 import { useEffect } from "react";
 
 const App = () => {
